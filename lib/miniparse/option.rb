09794157e5202a043ba39973218b9dc77f74a80e
Juanma Rodriguez <jmrod4@gmail.com>
2016-02-20 12:19:10 +0100

    class element (and option, optionswitch, optionflag) refactoring and tests done and functional

--- lib/miniparse/option.rb
+++ lib/miniparse/option.rb
@@ -6,80 +6,139 @@ module Miniparse
       s =~ /^(-?-?\w[\w-]*)/
       $1
     end
-  
+
     def self.get_name(s)
       s =~ /^-?-?(\w[\w-]*)/
       $1
     end
 
+    # external -> contract
     attr_reader :name
 
-    attr_reader :_short_help, :_desc, :_block
+    # internal -> can refactor any time
+    attr_reader :_short_help, :_description, :_block
 
-    def initialize(short_help, description, &block)
+    def initialize(short_help, description=nil, &block)
       @name = self.class.get_name(short_help).to_sym
       @_short_help = short_help
-      @_desc = description
+      @_description = description
       @_block = block
     end
 
+    # @return the recognizable part of the element if seen in a command line argument  
     def prefix
       n = self.class.get_prefix _short_help 
       #puts "sh: #{short_help} prefix: #{n}"
     end
-  
+
+    # runs the associated block
+    #
+    # @param arguments passed to the block
     def run(*args)
       _block.call(*args)    if _block 
     end
-  end
   
+  end
 
+end #module
+
+
+module Miniparse
+
+
+# TODO FEATURE consider using auto short options
+# TODO FEATURE consider be able to specify short options
+# TODO FEATURE consider doing unambiguous matches for shortened options
+# TODO FEATURE consider the option default value setting the type
   class Option < Interface
 
-    # external -> contract
     attr_reader :value
 
-    # internal -> can refactor any time
     attr_reader :_default, :_required
     
-    def initialize(short_help, description=nil, default=nil, required=false, &block)
+    def initialize(short_help, description=nil, default:nil, required:false, &block)
       super(short_help, description, &block)
       @value = @_default = default
       @_required = required
     end
-       
+
+    # runs the associated block (passing the option value to it)    
+    def run
+      super(value)
+    end
+    
+    # @param arg, like an element of ARGV
+    # @return true if arg corresponds to this option
+    def check(arg)
+      prefix == self.class.get_prefix(arg)
+    end
+
+    ############ PARSING
+    # allowed:
+    # 
+    # * for switches:
+    #   --option
+    #   --no-option
+    # * for flags
+    #   --option <value>
+    #   --option=<value>    
+    #
+    # FEATURE: <value> can be anything, even spaces 
+    # as for example if invoked in the command line as: "--option=    "
+    #
+    # raise exceptions:
+    # 
+    # * for switches and flags
+    #   --option= 
+    # * for flags
+    #   --option
+    # * for switches
+    #   --option=<value>
+    #   --option <value>
+    #
+    # @param arg, like an element of ARGV
+    # @return nil if arg doesn't correspond to this option 
+    # @return set value
+    def parse(arg)
+      return nil    unless check(arg) 
+      set_value(arg)
+    end
+
+    # extracts and sets the value from arg
+    #
+    # @param arg, like an element of ARGV
+    # @return set value    
     def set_value(arg)
       arg_name, val = self.class.format_to_name_value(arg)
       raise "can't get value from invalid format option '#{arg}'"    if val.nil?
       raise "#{name} option expected '#{arg}'"    if name.to_s != arg_name 
       @value = val
     end
-
-    def run
-      super(value)
+    
+    # @return an option formal specification like the ones on usage string
+    def specification
+      s = _short_help.strip
+      s = '[' + s + ']'    unless _required
     end
-  
-    #returns true if arg corresponds to this option
-    def check(arg)
-      prefix == self.class.get_prefix(arg)
-    end
-
-    def parse(arg)
-      return nil    unless check(arg) 
-      set_value(arg)
-    end
+    
   end
-  
-  
+      
+end # module
+
+
+module Miniparse
+
   class OptionSwitch < Option
-  
-    # return an array with name and value  
+
+    # @param s, a string (like an element of ARGV)
+    # @return an array with extracted option name and value  
     def self.format_to_name_value(s)
       s =~ /^--(no-)?(\w[\w-]*)$/
       [$2, $1.nil?]
     end
 
-    # return true if format correct
+    # @param s, a string (like an element of ARGV)
+    # @return true if format correct for a switch
     def self.valid_format(s)
       name, val = format_to_name_value(s)
       !name.nil?
@@ -91,105 +150,31 @@ module Miniparse
       #remove negative prefix and try to check again
       super("--#{$1}")
     end
+    
+    # TODO consider adding "[no-]" to the specification
+    
   end
-  
+end # module
 
+
+module Miniparse
   class OptionFlag < Option
-  
-    # return an array with name and value  
+    
+    # @param s, a string (like an element of ARGV)
+    # @return an array with extracted option name and value  
     def self.format_to_name_value(s)
       s =~ /^--(\w[\w-]*)[ |=](.+)$/
       [$1, $2]
     end
 
-    # return true if format correct
+    # @param s, a string (like an element of ARGV)
+    # @return true if format correct for a flag
     def self.valid_format(s)
       name, val = format_to_name_value(s)
       !name.nil?
     end
-  
+    
   end
 
-end
+end #module
 
-
-# module Miniparse
-# # NOTE
-# # the option default value set the type
-
-  # class Option
-    
-    # attr_reader :short_help, :value, :desc
-    # attr_reader :name, :is_switch
-
-    # def initialize(short_help, default=nil, description=nil, &block)
-      # @short_help = short_help 
-      # process_short_help
-      # @value = default
-      # @value_class = default.class
-      # @desc = description
-      # @block = block
-    # end
-
-    # def process_short_help
-      # if @short_help =~ /^--([\w\-]+)[ |=]\w+$/
-        # @name = $1.to_sym
-	      # @is_switch = false
-      # elsif @short_help =~ /^--([\w\-]+)$/
-        # @name = $1.to_sym
-	      # @is_switch = true
-      # else 
-        # raise "unknown format for option short help: #{@short_help}"
-      # end
-    # end
-   
-    # def run
-      # @block.call @value   if @block
-    # end 
-   
-    # ############ PARSING
-    
-    # # allowed:
-    # # for switches:
-    # #   --option
-    # #   --no-option
-    # # for flags
-    # #   --option <value>
-    # #   --option=<value>    
-
-    # # FEATURE: <value> can be anything, even spaces 
-    # #  as for example if invoked in the command line as: "--option=    "
-    
-    # # raise exceptions:
-    # # for switches and flags
-    # #   --option= 
-    # # for flags
-    # #   --option
-    # # for switches
-    # #   --option=<value>
-    # #   --option <value>
-    
-    # # TODO FEATURE: consider using auto short options
-    # # TODO FEATURE: consider be able to specify short options
-    # # TODO FEATURE: consider doing unambiguous matches for shortened options
-    
-    # def matches?(s)
-      # # returns option value or nil if s doesn't match this option
-      
-      # if s =~ /^--(no-)?#{@name}$/
-        # raise "missing value for option '#{@name}'"    unless @is_switch
-        # @value = $1.nil?
-      # elsif s =~ /^--#{@name}[ |=](.*)$/
-        # raise "value specified for switch option '#{name}'"    if @is_switch
-        # raise "missing value for flag option '#{name}'"    if $1 == ""
-        # @value = $1
-      # else
-        # # not this option
-        # nil
-      # end
-
-    # end
-    
-  # end
-
-# end
--- test/class_element_and_children_test.rb
+++ test/class_element_and_children_test.rb
@@ -14,17 +14,68 @@ class InterfaceTest < Minitest::Test
     s = "command"
     assert_equal "command", Interface.get_prefix(s)
     assert_equal "command", Interface.get_name(s)
+    s = "command stupid"
+    assert_equal "command", Interface.get_prefix(s)
+    assert_equal "command", Interface.get_name(s)
   end
 
+  def test_prefix_and_name
+    o = Interface.new "--option"
+    assert_equal "--option", o.prefix
+    assert_equal :option, o.name
+    
+    o = Interface.new "--option VALUE"
+    assert_equal "--option", o.prefix
+    assert_equal :option, o.name
+
+    o = Interface.new "command"
+    assert_equal "command", o.prefix
+    assert_equal :command, o.name
+  end
+  
+  def test_run
+    o = Interface.new "--option"
+    o.run
+
+    a = 8
+    o = Interface.new("--option") { a = a / 2 }
+    o.run
+    assert_equal 4, a
+  end
+  
 end
 
 
 class OptionTest < Minitest::Test
  
-  def test_simple_methods
-    o = Option.new "--option VALUE"
-    assert_equal :option, o.name
-    assert_equal "--option", o.prefix
+  def test_run
+    a = 8
+    o = Option.new("--option") { |val| a = val }
+    o.run
+    assert_equal nil, a
+  
+    a = 8
+    o = Option.new("--option", nil, default:true) { |val| a = val }
+    o.run
+    assert_equal true, a
+  end
+  
+  def test_parse_and_set_value
+    # functional only in child classes
+  end
+
+  def specification
+    o = OptionSwitch.new "--option"
+    assert_equal "[--option]", o.specification
+
+    o = OptionSwitch.new "--option", required:true
+    assert_equal "--option", o.specification 
+
+    o = OptionSwitch.new "--option VALUE"
+    assert_equal "[--option VALUE]", o.specification
+
+    o = OptionSwitch.new "--option VALUE", required:true
+    assert_equal "--option VALUE", o.specification 
   end
 
 end
@@ -50,7 +101,7 @@ class OptionSwitchTest < Minitest::Test
     assert_equal false, OptionSwitch.valid_format("--option VALUE")
  end
 
-  def test_value
+  def test_set_value
     o = OptionSwitch.new "--option"
     assert_equal true, o.set_value("--option")
     assert_equal false, o.set_value("--no-option")
@@ -58,25 +109,12 @@ class OptionSwitchTest < Minitest::Test
     assert_raises(RuntimeError) { o.set_value("--other") }
     assert_raises(RuntimeError) { o.set_value("other") }
   end
-
-  def test_run
-    o = OptionSwitch.new "--option"
-    o.run
-
-    a = 8
-    o = OptionSwitch.new("--option") { a = a / 2 }
-    o.run
-    assert_equal 4, a
-
-    a = 8
-    o = OptionSwitch.new("--option") { |val| a = val }
-    o.run
-    assert_equal nil, a
   
-    a = 8
-    o = OptionSwitch.new("--option", "", true) { |val| a = val }
-    o.run
-    assert_equal true, a
+  def test_check
+    o = OptionSwitch.new "--option"
+    assert_equal false, o.check("--other")
+    assert_equal true, o.check("--option")
+    assert_equal true, o.check("--no-option")
   end
 
   def test_parse
@@ -104,7 +142,7 @@ class OptionFlagTest < Minitest::Test
 
   end
 
-  def test_value
+  def test_set_value
     o = OptionFlag.new "--option"
     assert_equal "VALUE", o.set_value("--option VALUE")
     assert_equal "1", o.set_value("--option=1")
@@ -112,13 +150,22 @@ class OptionFlagTest < Minitest::Test
     assert_raises(RuntimeError) { o.set_value("--option") }
   end
 
+  def test_check
+    o = OptionFlag.new "--option VALUE"
+    assert_equal false, o.check("--other")
+    assert_equal true, o.check("--option")
+    assert_equal true, o.check("--option myfile")
+    assert_equal false, o.check("--no-option")
+  end
+
   def test_parse
     o = OptionFlag.new "--option FILE"
-    assert_equal nil, o.parse("--other")
-    assert_raises(RuntimeError) { o.parse("--option") }
-    #assert_raises(RuntimeError) { o.parse("--no-option") }
+    assert_equal nil, o.parse("--other")   
     assert_equal "1", o.parse("--option 1")
     assert_equal "file.txt", o.parse("--option file.txt")
+    assert_raises(RuntimeError) { o.parse("--option") }
+    skip "TODO consider to return an error"
+    assert_raises(RuntimeError) { o.parse("--no-option") }
   end
 
 end
