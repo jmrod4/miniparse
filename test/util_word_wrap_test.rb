4f530fc5d89514a00f4c190a8b7f263b214188d2
Juanma Rodriguez <jmrod4@gmail.com>
2016-03-19 13:29:07 +0100

    fixed minitest tests

--- test/util_word_wrap_test.rb
+++ test/util_word_wrap_test.rb
@@ -10,9 +10,9 @@ mancha,
 de cuyo nombre no
 quiero acordarme."
 
-    assert_equal(result, Miniparse.word_wrap(text, 20))
-    assert_equal(result+"\n", Miniparse.word_wrap(text+"\n", 20))
-    assert_equal("\n"+result, Miniparse.word_wrap("\n"+text, 20))
+    assert_equal(result, WordWrap.word_wrap(text, 20))
+    assert_equal(result+"\n", WordWrap.word_wrap(text+"\n", 20))
+    assert_equal("\n"+result, WordWrap.word_wrap("\n"+text, 20))
     
     result = "\
 En un lugar de la
@@ -20,7 +20,7 @@ mancha, de cuyo
 nombre no quiero
 acordarme."
 
-    assert_equal(result, Miniparse.word_wrap(text, 20, reformat:true))
+    assert_equal(result, WordWrap.word_wrap(text, 20, reformat:true))
     
     result2 = "\
 En un lugar de la   |En un lugar de la
@@ -29,7 +29,7 @@ de cuyo nombre no   |de cuyo nombre no
 quiero acordarme.   |quiero acordarme."
 
     assert_equal(result2, 
-        Miniparse.two_cols_word_wrap(text, '|', text, 20, 20))
+        WordWrap.two_cols_word_wrap(text, '|', text, 20, 20))
         
     result2 = "\
 En un lugar de la   | En un lugar de la
@@ -38,7 +38,7 @@ nombre no quiero    | nombre no quiero
 acordarme.          | acordarme."
 
     assert_equal(result2, 
-        Miniparse.two_cols_word_wrap(text, '| ', text, 20, 20, reformat:true))
+        WordWrap.two_cols_word_wrap(text, '| ', text, 20, 20, reformat:true))
   end
 
 end
