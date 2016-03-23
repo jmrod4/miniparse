14a9119d671db0f7e949a1686c49a678d743eb57
Juanma Rodriguez <jmrod4@gmail.com>
2016-03-23 14:07:45 +0100

    doc update

--- publish.sh
+++ publish.sh
@@ -1,6 +1,12 @@
+cp -r ../../prj/miniparse/doc/* .
+perl -i -pe 's/_index\.html/top-index.html/g' ./*
 perl -i -pe 's/_index\.html/top-index.html/g' css/*
 perl -i -pe 's/_index\.html/top-index.html/g' js/*
 perl -i -pe 's/_index\.html/top-index.html/g' Miniparse/*
 perl -i -pe 's/_index\.html/top-index.html/g' ./*
-mv top-index.html top-index.html
+mv _index\.html top-index.html
+git checkout gh-pages
+git add .
+git commit -m "doc update"
+git push
 
